5c229ace3741811b9514b31df2f2072107d2fdd5
Truong Cao Van <truongcvgcd210316@fpt.edu.vn>
2023-12-26 21:29:17 +0700

    Updated multiple files

--- Assignment2/BigSystem.java
+++ Assignment2/BigSystem.java
@@ -16,7 +16,7 @@ public class BigSystem {
         this.processingStack=processingStack;
     }
 
-     private String name;
+    private String name;
 
     public void setName(String name) {
         this.name = name;
@@ -33,6 +33,7 @@ public class BigSystem {
             // Printing connection information only for systemA connecting to systemB
             System.out.println(this.getClass().getSimpleName() + " '" + this.getName() + "' is connecting to " +
                     system.getClass().getSimpleName() + " '" + system.getName() + "'");
+            connected= true;
         } else if (this.getName().equals("systemB") && system.getName().equals("systemA")) {
             this.connectedSystem = system;
             system.connectedSystem = this;
@@ -40,6 +41,7 @@ public class BigSystem {
             // Optionally print a message indicating that the connection is not supported
             System.out.println(this.getClass().getSimpleName() + " '" + this.getName() + "' is connecting to " +
                     system.getClass().getSimpleName() + " '" + system.getName() + "'");
+            connected= true;
         }
     }
 
@@ -56,9 +58,6 @@ public class BigSystem {
     public Queue<String> getInBoxQueue() {
         return inBoxQueue;
     }
-    public Stack<String> getProcessingStack(){
-        return processingStack;
-    }
 
 
     public void sendMessage(String message) {
@@ -67,10 +66,11 @@ public class BigSystem {
                 throw new IllegalStateException("Error: Not connected to any system.");
             }
 
-            if (this == connectedSystem) {
+            if (connected) {
                 if (message.isEmpty()) {
                     throw new IllegalArgumentException("Error: Message is empty.");
                 }
+                // Xử lý tin nhắn từ SystemB đến SystemA
                 if (message.length() > 250) {
                     System.out.println("Error: Message is too long. Truncating message.");
                     int chunkSize = 250;
@@ -92,18 +92,25 @@ public class BigSystem {
         }
     }
 
+
+
     public void receiveMessageFromSystem(BigSystem connectedSystem) {
-        Queue<String> connectedInBoxQueue = connectedSystem.getInBoxQueue();
-        if (!connectedInBoxQueue.isEmpty()) {
-            while (!connectedInBoxQueue.isEmpty()) {
-                String message = connectedInBoxQueue.poll();
+        Queue<String> connectedOutBoxQueue = connectedSystem.getOutBoxQueue();
+
+        // Kiểm tra xem hàng đợi của hệ thống kết nối có tin nhắn hay không
+        if (!connectedOutBoxQueue.isEmpty()) {
+            // Nếu có tin nhắn, sao chép từ hàng đợi của hệ thống kết nối sang hàng đợi của hệ thống hiện tại
+            while (!connectedOutBoxQueue.isEmpty()) {
+                String message = connectedOutBoxQueue.poll();
                 this.inBoxQueue.offer(message);
             }
             System.out.println("Received messages from " + connectedSystem.getClass().getSimpleName());
         } else {
-            System.out.println(connectedSystem.getClass().getSimpleName() + "'s Inbox Queue is empty.");
+            // Nếu hàng đợi của hệ thống kết nối rỗng, thông báo rằng không có tin nhắn
+            System.out.println(connectedSystem.getClass().getSimpleName() + "'s Outbox Queue is empty.");
         }
     }
+
 
     public void sendRequestToReceive() {
         if (isConnected()) {
@@ -132,7 +139,7 @@ public class BigSystem {
 
 
     public void readOutboxQueue() {
-        System.out.println("Outgoing Messages from SystemA:");
+        System.out.println("Outgoing Messages from " + this.getClass().getSimpleName() + " '" + this.getName() + "':");
         if (outBoxQueue.isEmpty()) {
             System.out.println("Outbox Queue is empty.");
         } else {
@@ -142,9 +149,8 @@ public class BigSystem {
         }
     }
 
-
     public void readInboxQueue() {
-        System.out.println("Incoming Messages to System:");
+        System.out.println("Incoming Messages to " + this.getClass().getSimpleName() + " '" + this.getName() + "':");
         if (inBoxQueue.isEmpty()) {
             System.out.println("Inbox Queue is empty.");
         } else {
@@ -156,11 +162,21 @@ public class BigSystem {
 
 
     public void processMessages() {
-        if (!inBoxQueue.isEmpty()) {
-            String message = inBoxQueue.poll();
-            processingStack.push(message);
+        try {
+            if (connectedSystem == null) {
+                throw new IllegalStateException("Error: Connection not established.");
+            }
+
+            while (!inBoxQueue.isEmpty()) {
+                String message = inBoxQueue.poll();
+                processingStack.push(message);
+                System.out.println("Message processed: " + message);
+            }
+        } catch (IllegalStateException e) {
+            System.out.println(e.getMessage());
         }
     }
+
 
 
     public void disconnect(BigSystem systemToDisconnect) {
